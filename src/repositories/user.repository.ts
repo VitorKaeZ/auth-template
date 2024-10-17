b079198740efbf9faded8e5f6828978f234e0f10
VitorKaeZ <vitorcaetanoz@outlook.com>
2024-10-17 07:31:22 -0300

    feat: user repository logic

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,8 +1,23 @@
+import { database } from "../database/prisma-client";
 import { User, UserCreate, UserRepository } from "../interfaces/user.interface";
+import bcrypt from "bcryptjs"
 
 class UserRepositoryDatabase implements UserRepository {
     async create(data: UserCreate): Promise<User> {
         
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(data.password, salt)
+            
+        
+        const result = await database.user.create({
+            data: {
+                firstname: data.firstname,
+                lastname: data.lastname,
+                email: data.email,
+                password: passwordHash
+            }
+        })
+        return result
     }
 }
 

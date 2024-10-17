384e394074943dfe52c2c60102a8a5d1d59de3d8
VitorKaeZ <vitorcaetanoz@outlook.com>
2024-10-17 18:04:36 -0300

    feat: prisma repository

---
+++ src/external/prisma-user-repository.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from '@prisma/client';
+import { UserRepository } from '../usecases/ports/user-repository';
+import { UserData } from '../entities/user/user-data';
+
+const prisma = new PrismaClient();
+
+export class PrismaUserRepository implements UserRepository {
+  async findAllUsers(): Promise<UserData[]> {
+    // Busca todos os usuários no banco de dados
+    return await prisma.user.findMany();
+  }
+
+  async findUserByEmail(email: string): Promise<UserData | null> {
+    // Busca um usuário pelo e-mail
+    const result = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+      select: {
+        id: true,
+        firstname: true,
+        lastname: true,
+        email: true,
+        password: true,
+      },
+    });
+    
+    return result
+  }
+
+  async add(user: UserData): Promise<void> {
+    // Verifica se o usuário já existe pelo e-mail
+    const exists = await this.exists(user.email);
+    if (!exists) {
+      // Adiciona o usuário se ele não existir
+      await prisma.user.create({
+        data: user,
+      });
+    }
+  }
+
+  async exists(email: string): Promise<boolean> {
+    // Verifica se o usuário existe pelo e-mail
+    const user = await this.findUserByEmail(email);
+    return user !== null;
+  }
+}

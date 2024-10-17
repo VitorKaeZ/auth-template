3820154ab1850e7ad3a5589fabb0c00b3a42a20b
VitorKaeZ <vitorcaetanoz@outlook.com>
2024-10-17 18:04:50 -0300

    feat: add register factories

---
+++ src/main/factories/register.ts
@@ -0,0 +1,11 @@
+import { RegisterUserController } from "../../adapters/presentation/controllers/user.controller"
+import { PrismaUserRepository } from "../../external/prisma-user-repository"
+import { RegisterUserOnDatabase } from "../../usecases/user/register-user-on-database"
+
+
+export const makeRegisterUserController = (): RegisterUserController => {
+    const prismaUserRepository = new PrismaUserRepository()
+    const registerUserOnDatabase = new RegisterUserOnDatabase(prismaUserRepository)
+    const registerUserController = new RegisterUserController(registerUserOnDatabase)
+  return registerUserController
+}

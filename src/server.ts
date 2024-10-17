a0d90d342c862244c33af643a86a38672bfaeb67
VitorKaeZ <vitorcaetanoz@outlook.com>
2024-10-17 07:30:54 -0300

    feat: add dotenv and bcrypt

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,24 @@
 import fastify from "fastify"
 import cors from "@fastify/cors";
 import { routes } from "./routes/routes";
+import { userRoutes } from "./routes/user.routes";
+import "dotenv/config"
 const app = fastify({logger: false})
 
 
 const start = async () => {
     
     await app.register(cors)
+
     await app.register(routes)
+
+    await app.register(userRoutes, {
+        prefix: '/users',
+    })
 
 
     try {
-        await app.listen({ port: 3333 }).then(() =>{
+        await app.listen({ port: Number(process.env.PORT) }).then(() =>{
             console.log("Server is running!")
         })
     } catch (error) {

962692f666afb0648de54e184b9471c64442d661
VitorKaeZ <vitorcaetanoz@outlook.com>
2024-10-18 00:45:48 -0300

    feat: userdata for request and response for login user

--- src/entities/user/user-data.ts
+++ src/entities/user/user-data.ts
@@ -1,6 +1,19 @@
 export interface UserData {
+    id?: string,
     email: string;
     firstname: string;
     lastname: string;
     password: string;
+}
+
+export interface UserDataLoginRequest {
+    email: string;
+    password: string
+}
+export interface UserDataLoginResponse {
+    id: string | undefined,
+    email: string;
+    firstname: string;
+    lastname: string;
+    token: string;
 }

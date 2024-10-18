9d361d6048c8779ce168cd844cb5bac4c30284d1
VitorKaeZ <vitorcaetanoz@outlook.com>
2024-10-18 00:47:28 -0300

    feat: user login factories

---
+++ src/main/factories/login.ts
@@ -0,0 +1,12 @@
+import { LoginUserController } from "../../adapters/presentation/controllers/login-user-controller"
+import { PrismaUserRepository } from "../../external/prisma-user-repository"
+import { LoginUserOnService } from "../../usecases/user/login-user-on-service"
+
+
+export const makeLoginUserController = (): LoginUserController => {
+    const prismaUserRepository = new PrismaUserRepository()
+    const loginUserOnService = new LoginUserOnService(prismaUserRepository)
+    const loginUserController = new LoginUserController(loginUserOnService)
+    
+    return loginUserController
+}

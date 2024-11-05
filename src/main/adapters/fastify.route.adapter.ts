3e9352aac634f7e7b884f29cb89aefd998c32707
VitorKaeZ <vitorcaetanoz@outlook.com>
2024-11-05 13:21:32 -0300

    fix: adaptRoute controller type

--- src/main/adapters/fastify.route.adapter.ts
+++ src/main/adapters/fastify.route.adapter.ts
@@ -2,8 +2,10 @@ import { RegisterUserController } from '../../interfaces/controllers/register-us
 import { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } from "fastify";
 import { HttpRequest } from '../../interfaces/controllers/ports/http'
 import { LoginUserController } from '../../interfaces/controllers/login-user-controller';
+import { PasswordResetController } from '../../interfaces/controllers/PasswordResetController';
+import { RequestPasswordResetController } from '../../interfaces/controllers/RequestPasswordResetController';
 
-export const adaptRoute = (controller: RegisterUserController | LoginUserController) => {
+export const adaptRoute = (controller: RegisterUserController | LoginUserController | PasswordResetController | RequestPasswordResetController) => {
   return async (req: FastifyRequest, reply: FastifyReply) => {
     const httpRequest: HttpRequest = {
       body: req.body

0cf49a05aabccc24f3b21fd76313d8c00cb3bf54
VitorKaeZ <vitorcaetanoz@outlook.com>
2024-11-05 13:14:36 -0300

    feat: add requestpassword usecases

---
+++ src/application/usecases/passwordReset/requestPasswordResetOnService.ts
@@ -0,0 +1,43 @@
+import { IPasswordResetRepository } from "../../../domain/repositories/user/IPasswordResetRepository";
+import { UserRepository } from "../../../domain/repositories/user/IUserRepository";
+import { Either, left, right } from "../../../shared/either";
+import { InvalidOrExpiredTokenError } from "../errors/invalidOrExpiredTokenError";
+import { PasswordReset, PasswordResetInterface } from "./passwordReset";
+import { addHours, isAfter } from "date-fns";
+import bcrypt from "bcryptjs"
+import { IEmailService } from "../../services/IEmailService";
+import { RequestPasswordResetInterface } from "./requestPasswordReset";
+import crypto from "crypto";
+
+export class RequestPasswordReset implements RequestPasswordReset {
+    private userRepository: UserRepository;
+    private passwordResetRepository: IPasswordResetRepository;
+    private emailService: IEmailService;
+
+  constructor(userRepo: UserRepository, passwordRepo: IPasswordResetRepository, emailService: IEmailService) {
+    this.userRepository = userRepo;
+    this.passwordResetRepository = passwordRepo;
+    this.emailService = emailService;
+  }
+   
+
+    async reqPasswordResetOnService ({email}:RequestPasswordResetInterface): Promise<Either<InvalidOrExpiredTokenError, RequestPasswordResetInterface>>{
+        const user = await this.userRepository.findUserByEmail(email)
+
+        if (!user || !user.id) {
+          return left(new InvalidOrExpiredTokenError())
+
+        }
+
+        const token = crypto.randomBytes(32).toString("hex")
+        const expiresAt = addHours(new Date(), 0.10)
+
+        await this.passwordResetRepository.createToken(user.id, token, expiresAt)
+
+        const resetLink = `${process.env.FRONTEND_URL}/reset-password?token=${token}`
+
+        await this.emailService.sendEmail(user.email, "Password Reset", `Click Here: ${resetLink}`)
+
+        return right({email})
+    }
+}
